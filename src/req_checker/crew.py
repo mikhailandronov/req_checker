6c973f5a504da78f17f0a9a26b8ae8cad6541e70
Mikhail Andronov <mikhail_andronov@mail.ru>
2025-05-19 23:07:00 +0000

    Get complete check-list on reqs

--- src/req_checker/crew.py
+++ src/req_checker/crew.py
@@ -2,53 +2,222 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
-from crewai_tools import SerperDevTool, WebsiteSearchTool
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+from crewai_tools import SerperDevTool #, DOCXSearchTool
 
 @CrewBase
 class ReqChecker():
-    """ReqChecker crew"""
 
     agents: List[BaseAgent]
     tasks: List[Task]
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
     def req_methodologist(self) -> Agent:
         search_tool = SerperDevTool()
-        # web_rag_tool = WebsiteSearchTool()
         return Agent(
             config=self.agents_config['req_methodologist'], # type: ignore[index]
             verbose=True,
             tools=[search_tool]
         )
+    
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
     @task
     def req_methodology_task(self) -> Task:
         return Task(
             config=self.tasks_config['req_methodology_task'], # type: ignore[index]
         )
+    
+
+    def req_expert(self, aspect_questions) -> Agent:
+        search_tool = SerperDevTool()
+     
+        return Agent (
+            role="Главный эксперт по аспекту '{aspect}' требований к информационным системам".format(**aspect_questions),
+            goal="""Уточнить и дополнить список вопросов по аспекту '{aspect}' требований к информационным системам.
+                    Определить все ключевые вопросы по аспекту '{aspect}', ответы на которые обязательно должны быть включены в качественную 
+                    спецификацию требований (техническое задание (ТЗ, SOW), запрос предложений (RFP)) 
+                    для выбора поставщика (исполнителя работ) на разработку информационной системы.""".format(**aspect_questions),
+            backstory="""Ты опытный эксперт, много лет отвечающий за спецификацию требований по аспекту '{aspect}'. 
+                    Ты в деталях знаешь как общие стандарты, такие как ISO/IEC/IEEE 29148, 
+                    ГОСТ Р ИСО/МЭК/IEEE 29148-2014, ГОСТ 34, так и специализированные стандарты, нормы, правила и лучшие практики 
+                    по аспекту '{aspect}'. 
+                    Ты известен тем, что можешь обеспечить полноту требований и точность формулировок. 
+                    Ты можешь дать исчерпывающий список вопросов, определяющих то, какие требования должны быть указаны, чтобы 
+                    гарантированно выбрать наилучшее предложение среди потенциальных подрядчиков. """.format(**aspect_questions),
+            verbose=True,
+            tools=[search_tool]
+        )
+    
+
+    
+    def req_details_task(self, aspect_questions, agent) -> Task:
+        # return Task(
+        #     config=self.tasks_config['req_details_task'], # type: ignore[index]
+        # )
+        desc = ("Сформировать ИСЧЕРПЫВАЮЩИИЙ список вопросов по аспекту '{aspect}' требований к информационной системе для "
+        "проведения конкурсной процедуры выбора поставщика (исполнителя всех работ по разработке этой системы). "
+        "Для этого можно взять за основу вопросы, предложенные коллегами: {questions}. Подумай, насколько они важны и конкретны."
+        "Важно проверить их корректность, дополнить, уточнить, детализировать, опираясь на свою экспертизу в аспекте '{aspect}'. "
+        "Итоговый список должен содержать ВСЕ вопросы по этому аспекту, на которые надо ответить в документе с требованиями, "
+        "чтобы гарантированно выбрать наилучшее предложение среди потенциальных подрядчиков. "
+        "При необходимости можно поискать информацию в интернете, но важно критически оценить результаты поиска."
+        "Если вопрос не является критичным для отбора поставщика, еге не нужно указывать."
+        "Вернуть результат ИСКЛЮЧИТЕЛЬНО в виде Python словаря с элементами 'aspect' и 'questions'. "
+        "Никаких других слов или символов, кроме словаря Python, в ответе быть не должно! "
+        "Например: "
+        "{{'aspect':'{aspect}', "
+        "'questions':['Какой протокол используется для интеграции систем?', "
+        "'Какая сторона является инициатором взаимодействия?', "
+        "'Как защищаются поток данных при передаче?'"
+        "]}}")
+        
+        print ("desc ||| ", desc.format(**aspect_questions))
+
+        eo = ("Python словарь с элементами 'aspect' и 'questions', где 'questions' - это словарь строк с вопросами. "
+        "Никаких других слов или символов, кроме словаря Python, в ответе быть не должно! "
+        "Пример: "
+        "{{'aspect':'{aspect}', "
+        "'questions':['Какой протокол используется для интеграции систем?', "
+        "'Какая сторона является инициатором взаимодействия?', "
+        "'Как защищаются поток данных при передаче?'"
+        "]}}")
+
+        print ("eo ||| ", eo.format(**aspect_questions))
+
+        return Task(
+            description=desc.format(**aspect_questions),
+            expected_output=eo.format(**aspect_questions),
+            agent=agent
+            )
+        
+
+    def req_checklist_builder(self) -> Agent:
+        return Agent(
+            role="Главный составитель сводных чек-листов",
+            goal="""Собрать воедино все Python словари, полученные от экспертов по каждому из аспектов требований, какие есть в контексте,
+                и составить из них единый, полный итоговый список аспектов с вопросами - чек-лист.
+                """,
+            backstory=(
+                "Вы мастерски объединяете полученные от экспертов блоки в один большой список."
+                " Вы никогда не меняете и не теряете полученные элементы при объединении их в список."
+            ),
+            verbose=True
+        )
+    
+    def req_checklist_build_task(self, expert_tasks) -> Task:
+        return Task(
+            description=(
+                """Собрать воедино все Python словари, полученные от экспертов по каждому из аспектов требований, какие есть в контексте,
+                и составить из них единый, полный итоговый список аспектов с вопросами - чек-лист.
+                Вернуть результат ИСКЛЮЧИТЕЛЬНО в виде Python списка, содержащего словари с элементами 'aspect' и 'questions'.
+                Никаких других слов или символов, кроме списка Python, в ответе быть не должно! 
+                В результат должны попасть ВСЕ полученные от экспертов по аспектам словари. Ничего не должно пропасть!
+                Например:
+                [
+                {'aspect':'Требования к интеграции', 'questions':['С какими системами интегрируется?','Какие данные передаются?', 'Какой протокол используется?']},
+                {'aspect':'Требования к плану работ', 'questions':['Каковы этапы проекта?', 'Каковы результаты каждого этапа?']},
+                {'aspect':'Требования к приемке и оплате', 'questions':['Как осуществляется приемка?','Каковы критерии выполнения работы?','Какие документы подтверждают выполнение работ и приемку?', 'Когда производится оплата?']}
+                ]
+                """
+            ),
+            expected_output=(
+                """Python список, содержащий словари с элементами 'aspect' и 'questions'.
+                Никаких других слов или символов, кроме списка Python, в ответе быть не должно! 
+                В результат должны попасть ВСЕ полученные от экспертов по аспектам словари. Ничего не должно пропасть!
+                Например:
+                [
+                {'aspect':'Требования к интеграции', 'questions':['С какими системами интегрируется?','Какие данные передаются?', 'Какой протокол используется?']},
+                {'aspect':'Требования к плану работ', 'questions':['Каковы этапы проекта?', 'Каковы результаты каждого этапа?']},
+                {'aspect':'Требования к приемке и оплате', 'questions':['Как осуществляется приемка?','Каковы критерии выполнения работы?','Какие документы подтверждают выполнение работ и приемку?', 'Когда производится оплата?']}
+                ]"""
+            ),
+            agent=self.req_checklist_builder(),
+            context=expert_tasks 
+        )
 
     @crew
-    def crew(self) -> Crew:
-        """Creates the ReqChecker crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+    def methodology_crew(self) -> Crew:
+        return Crew(
+            agents=[self.req_methodologist()], 
+            tasks=[self.req_methodology_task()],
+            process=Process.sequential,
+            verbose=True
+        )
+
+
+    
+    def req_details_crew(self, aspects) -> Crew:
+        
+        req_experts = []
+        expert_tasks = []
+        
+        for aspect_desc in aspects:
+            print (">>>>> ",aspect_desc)
+            expert = self.req_expert(aspect_questions=aspect_desc)
+            req_experts.append(expert)
+            print (">> эксперт создан > ")
+            task = self.req_details_task(aspect_questions=aspect_desc, agent=expert)
+            expert_tasks.append(task)
+            print (">> таск создан > ")
 
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=req_experts + [self.req_checklist_builder()], 
+            tasks=expert_tasks + [self.req_checklist_build_task(expert_tasks=expert_tasks)],
             process=Process.sequential,
-            verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+            verbose=True
         )
+    
+    # def rag_expert(self, rag_tool, question) -> Agent:
+    #     return Agent (
+    #         role="Главный эксперт по поиску ответов на вопросы в контексте запроса",
+    #         goal="Найти в тексте запроса ответ на вопрос '{}', используя все доступные инструменты. ".format(question),
+    #         backstory="Ты опытный специалист по поиску ответов в тексте. Ты известен тем, что умеешь использовать инструменты для поиска в DOCX. ",
+    #         verbose=True,
+    #         tools=[rag_tool]
+    #     )
+    
+
+    
+    # def rag_task(self, aspect_questions, agent) -> Task:
+    #     # return Task(
+    #     #     config=self.tasks_config['req_details_task'], # type: ignore[index]
+    #     # )
+    #     desc = "Найти ответ на вопрос, используя доступные инструменты и информацию в запросе. Если ответа нет, надо вернуть строку 'Нет ответа'"
+        
+    #     eo = "Python словарь с элементами 'aspect' и 'questions', где 'questions' - это словарь строк с вопросами. "
+    #     "Никаких других слов или символов, кроме словаря Python, в ответе быть не должно! "
+    #     "Пример: "
+    #     "{'aspect':'{aspect}', "
+    #     "'questions':['Какой протокол используется для интеграции систем?', "
+    #     "'Какая сторона является инициатором взаимодействия?', "
+    #     "'Как защищаются поток данных при передаче?'"
+    #     "]}"
+
+    #     return Task(
+    #         description=desc.format(**aspect_questions),
+    #         expected_output=eo.format(**aspect_questions),
+    #         agent=agent
+    #         )
+
+
+    # def rag_crew(self, filepath, questions) -> Crew:
+        
+    #     rag_tool = DOCXSearchTool(filepath)
+    #     rag_experts = []
+    #     expert_tasks = []
+        
+    #     for question in questions:
+    #         print (">>>>> ",question)
+    #         expert = self.req_expert(aspect_questions=aspect_desc)
+    #         req_experts.append(expert)
+    #         print (">> эксперт создан > ")
+    #         task = self.req_details_task(aspect_questions=aspect_desc, agent=expert)
+    #         expert_tasks.append(task)
+    #         print (">> таск создан > ")
+
+    #     return Crew(
+    #         agents=req_experts + [self.req_checklist_builder()], 
+    #         tasks=expert_tasks + [self.req_checklist_build_task(expert_tasks=expert_tasks)],
+    #         process=Process.sequential,
+    #         verbose=True,
+    #         # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+    #     )
--- src/req_checker/main.py
+++ src/req_checker/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import sys
 import warnings
+import ast
 
 from datetime import datetime
+import typing as tp
 
 from req_checker.crew import ReqChecker
 
@@ -13,23 +15,92 @@ warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 # Replace with inputs you want to test with, it will automatically
 # interpolate any tasks and agents information
 
+def get_aspects(aspect_questions_string: str) -> list[dict[str, tp.Any]]:
+    
+    aspects_list = [{}]
+    try:
+        aspects_list = ast.literal_eval(aspect_questions_string.strip())
+        if not isinstance(aspects_list, list) or not all(isinstance(aspect, dict) for aspect in aspects_list):
+            raise ValueError("Результат методолога не является списком словарей.")
+    except (ValueError, SyntaxError) as e:
+        print(f"⚠️ Ошибка парсинга списка аспектов: {e}. Пожалуйста, проверьте вывод методолога.")
+        print("Используем запасной список аспектов для демонстрации.")
+        aspects_list = [
+            {
+                'aspect': 'Требования к функциональности системы',
+                'questions': [
+                    'Какие основные функции должна выполнять система?',
+                    'Какие бизнес-процессы автоматизирует система?',
+                    'Какие пользовательские роли и права должны быть реализованы?',
+                    'Должна ли система обладать возможностью масштабирования и расширения функциональности?',
+                    'Какие интеграции с другими системами обязательны?'
+                ]
+            },
+            {
+                'aspect': 'Требования к безопасности и защите данных',
+                'questions': [
+                    'Какие требования к хранению и передаче персональных данных?',
+                    'Какие меры защиты информации должны быть реализованы (шифрование, аутентификация, аудит)?',
+                    'Соответствует ли система нормативам по информационной безопасности (ФЗ 152, ГОСТ Р ИСО/МЭК/IEEE 27001)?',
+                    'Какие механизмы обнаружения и предотвращения несанкционированного доступа предусмотрены?',
+                    'Как реализована защита от внешних и внутренних угроз?'
+                ]
+            },
+            {
+                'aspect': 'Требования к интерфейсу и пользовательскому опыту',
+                'questions': [
+                    'Какие требования предъявляются к удобству и интуитивности интерфейса?',
+                    'Должна ли система поддерживать мобильные устройства и разные платформы?',
+                    'Какие языковые настройки должны быть реализованы?',
+                    'Какие требования к доступности для людей с ограниченными возможностями?',
+                    'Должны ли реализовываться функции обучения и поддержки пользователей?'
+                ]
+            }
+        ]
+    
+    return aspects_list
+
+# def run_process():
+#     """
+#     Run the crew.
+#     """
+
+#     filepath = "uploaded_files/FTT.docx" 
+#     questions = ['В чем цель проекта', 'Какая должность у Белинской']
+
+#     result = "Не удалось получить результат"
+
+#     try:
+#         result = ReqChecker().rag_crew(filepath=filepath, questions=questions).kickoff().raw
+#     except Exception as e:
+#         raise Exception(f"An error occurred while running the crew: {e}")
+    
+#     print(result)
+
 def run():
     """
     Run the crew.
     """
-    inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
-    }
-    
+
     result = "Не удалось получить результат"
 
     try:
-        result = ReqChecker().crew().kickoff(inputs=inputs).raw
+        result = ReqChecker().methodology_crew().kickoff().raw
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
     
-    print(result)
+    aspects = get_aspects(result)
+
+    try:
+        result = ReqChecker().req_details_crew(aspects= aspects).kickoff().raw
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+    print("Вопросы:\n", result)
+    aspects = get_aspects(result)
+    print("Итог:\n", aspects)
+    
+
 
 def run_in_streamlit(topic:str, current_year:int) -> str:
     """
